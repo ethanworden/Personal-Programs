4cb71c38d6618cfc6317b83492f7a4b6309bcd2e
ethanworden <46758520+ethanworden@users.noreply.github.com>
2020-05-27 00:12:39 -0700

    Add files via upload

---
+++ Object01/Object01/Object01.cs
@@ -0,0 +1,46 @@
+﻿/*Source:https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/objects
+ * Summary: Uses Object getters and setters to get and set age and names for "Person" objects
+ */
+
+using System;
+namespace Object01
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+      
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person person1 = new Person("Leopold", 6);
+            Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person1.Age);
+
+            // Declare new person, assign person1 to it.
+            Person person2 = person1;
+
+            // Change the name of person2, and person1 also changes.
+            person2.Name = "Molly";
+            person2.Age = 16;
+
+            Console.WriteLine("person2 Name = {0} Age = {1}", person2.Name, person2.Age);
+            Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person1.Age);
+
+        }
+    }
+  
+}
+/*
+   Output:
+   person1 Name = Leopold Age = 6
+   person2 Name = Molly Age = 16
+   person1 Name = Molly Age = 16
+*/
