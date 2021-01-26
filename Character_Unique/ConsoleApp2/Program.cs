1f0c99a4fb21656301bfff7159bedcf9f005cf40
ethanworden <46758520+ethanworden@users.noreply.github.com>
2021-01-26 14:56:16 -0800

    Add files via upload

---
+++ Character_Unique/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*This program allows a user to input a string and it will determine if all the characters are unique or not */
+
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static bool isUnique(string s)
+        {
+            char[] arr = s.ToCharArray();
+
+            for(int i = 0; i < arr.Length-1; i++)
+            {
+                for(int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[i] == arr[k])
+                    {
+                        Console.WriteLine($"Not all characters are unique!!! Repition with character: {arr[i]}");
+                        return false;
+                    }
+                }
+            }
+            Console.WriteLine("All characters are unique.");
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word: ");
+            string response = Console.ReadLine();
+            isUnique(response);
+            
+        }
+    }
+}
